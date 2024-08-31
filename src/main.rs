24c31f5e3baa0adb3b33f70d1a1d9170771abafa
IngAivar <MiniGan067@yandex.ru>
2024-08-31 16:25:32 +0300

    comments rework

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,41 @@
-use tokio::runtime::Runtime;
 use flume::{Sender, Receiver};
 use std::time::Duration;
 use tokio::signal;
 
+#[tokio::main]
+async fn main() {
+    // Создаем канал для передачи данных между задачами
+    let (tx, rx) = flume::bounded(10); // Создаем ограниченный канал с буфером на 10 элементов
+
+    // Запускаем задачу, которая будет отправлять значения в канал
+    tokio::spawn(async move {
+        send_values(tx.clone(), 100).await;
+    });
+
+    // Запускаем задачу, которая будет получать значения из канала
+    tokio::spawn(async move {
+        receive_values(rx).await;
+    });
+
+    // Ожидаем сигнала Ctrl+C для завершения программы
+    signal::ctrl_c().await.unwrap();
+    println!("Ctrl+C received, shutting down...");
+    println!("Program finished");
+}
+
 async fn send_values(tx: Sender<i32>, n: i32) {
+    // Отправляем значения в канал с задержкой
     for i in 1..=n {
         tx.send(i).unwrap();
-        let _ = tokio::time::sleep(Duration::from_secs(1)).await;
         println!("Sent: {}", i);
+        let _ = tokio::time::sleep(Duration::from_secs(1)).await;
     }
 }
 
 async fn receive_values(rx: Receiver<i32>) {
+    // Получаем значения из канала и выводим их
     for value in rx {
         println!("Received: {}", value);
         let _ = tokio::time::sleep(Duration::from_secs(1)).await;
     }
 }
-
-fn main() {
-    let rt = Runtime::new().unwrap();
-
-    let (tx, rx) = flume::bounded(10); // Creates channel with buffer
-
-    let n = 100;
-
-    rt.spawn(async move {
-        send_values(tx.clone(), n).await;
-    });
-
-    rt.spawn(async move {
-        receive_values(rx).await;
-    });
-
-    rt.block_on(async {
-        match signal::ctrl_c().await {
-            Ok(_) => {
-                println!("Ctrl+C received, shutting down...");
-            }
-            Err(err) => {
-                eprintln!("Error listening for Ctrl+C: {}", err);
-            }
-        }
-    });
-
-    println!("Program finished");
-}
-
-// Запуск через cargo run в связи с появлением библиотек
